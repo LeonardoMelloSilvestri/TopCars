79f58301cf9ed41ece4c2fcceb14dc4ab3afd10e
LeonardoMelloSilvestri <leonardomscontato@gmail.com>
2023-11-17 19:00:45 -0300

    Creating banner component

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+export default function Banner() {
+  return (
+    <>
+      <article className="flex flex-col items-center text-center shadow-2xl shadow-inner p-12 bg-orange-600 text-white mt-20">
+        <h1 className="text-4xl font-bold mb-3">
+          Batemos qualquer preço da concorrência!
+        </h1>
+        <h2 className="text-3xl font-semibold">
+          Converse com um de nossos vendedores
+        </h2>
+      </article>
+    </>
+  );
+}
