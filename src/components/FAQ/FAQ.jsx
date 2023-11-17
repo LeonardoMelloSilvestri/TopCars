80c408655df794c6c09c5de0bb28ae70bafef2ae
LeonardoMelloSilvestri <leonardomscontato@gmail.com>
2023-11-17 19:03:23 -0300

    Created FAQ component (under development)

---
+++ src/components/FAQ/FAQ.jsx
@@ -0,0 +1,18 @@
+export default function FAQ() {
+  return (
+    <>
+      <article className="flex flex-col items-center mt-20 mb-20">
+        <div className="w-11/12 lg:w-6/12 text-center">
+          <h2 className="text-2xl font-semibold mb-2">FAQ</h2>
+          <h1 className="text-4xl font-bold mb-6">Perguntas comuns</h1>
+          <h2 className="text-md text-slate-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+            delectus explicabo placeat a neque aliquam! Nobis ad sed quod
+            maxime? Obcaecati maiores totam placeat delectus similique
+            blanditiis nihil repellat commodi.
+          </h2>
+        </div>
+      </article>
+    </>
+  );
+}
